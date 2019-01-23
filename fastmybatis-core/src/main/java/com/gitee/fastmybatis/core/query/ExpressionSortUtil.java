086a5eeec554a9c18c54fd0ed83da46b07cee561
tanghc <thc8775@163.com>
2019-01-23 17:09:48 +0800

    代码优化

--- fastmybatis-core/src/main/java/com/gitee/fastmybatis/core/query/ExpressionSortUtil.java
+++ fastmybatis-core/src/main/java/com/gitee/fastmybatis/core/query/ExpressionSortUtil.java
@@ -10,7 +10,18 @@ import java.util.List;
  * @author tanghc
  */
 public class ExpressionSortUtil {
+
+    /**
+     * 给条件排序
+     * @param expressionList 条件列表
+     * @param <T> 表达式
+     * @return 返回排序后的列表
+     */
     public static <T extends Expression> List<T> sort(List<T> expressionList) {
+        if (expressionList == null) {
+            return null;
+        }
+
         Collections.sort(expressionList, new Comparator<Expression>() {
             @Override
             public int compare(Expression o1, Expression o2) {
